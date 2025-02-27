e3f188b0e722ea02a1b02071336077d44c8d3708
0xkiyoshi <kiyoshitaro98@gmail.com>
2025-02-27 18:30:40 +0700

    fix: change Birdeye token search sorting from marketcap to liquidity

--- packages/providers/birdeye/src/BirdeyeProvider.ts
+++ packages/providers/birdeye/src/BirdeyeProvider.ts
@@ -63,7 +63,7 @@ export class BirdeyeProvider implements ITokenProvider, IWalletProvider {
         headers: this.getHeaders(network),
         params: {
           keyword: query,
-          sort_by: 'marketcap',
+          sort_by: 'liquidity',
           sort_type: 'desc',
           limit: 10,
           chain: this.mapChain(network),
