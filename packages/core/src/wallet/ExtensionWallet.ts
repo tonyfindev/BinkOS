1a16e9957f18818f64045449828dd688160c6ff0
tommyjhs <tommyjohnson1998n@gmail.com>
2025-05-07 10:09:39 +0700

    refactor: increase timeout duration for socket operations

--- packages/core/src/wallet/ExtensionWallet.ts
+++ packages/core/src/wallet/ExtensionWallet.ts
@@ -19,7 +19,7 @@ import {
 export class ExtensionWallet implements IWallet {
   socket: Socket | null = null;
   readonly #network: Network;
-  readonly timeout: number = 30000; // 30 seconds timeout
+  readonly timeout: number = 60000; // 60 seconds timeout
 
   constructor(network: Network) {
     this.#network = network;
@@ -48,7 +48,9 @@ export class ExtensionWallet implements IWallet {
   public async getAddress(network: NetworkName): Promise<string> {
     await this.ensureConnection();
 
-    const response = (await this.socket?.timeout(5000).emitWithAck('get_address', { network })) as {
+    const response = (await this.socket
+      ?.timeout(this.timeout)
+      .emitWithAck('get_address', { network })) as {
       address?: string;
       error?: string;
     };
@@ -64,7 +66,9 @@ export class ExtensionWallet implements IWallet {
   public async signMessage(params: SignMessageParams): Promise<string> {
     await this.ensureConnection();
 
-    const response = (await this.socket?.timeout(5000).emitWithAck('sign_message', params)) as {
+    const response = (await this.socket
+      ?.timeout(this.timeout)
+      .emitWithAck('sign_message', params)) as {
       signature?: string;
       error?: string;
     };
@@ -90,7 +94,7 @@ export class ExtensionWallet implements IWallet {
       transactionStr = Buffer.from(params.transaction.serialize()).toString('base64');
     }
 
-    const response = (await this.socket?.timeout(5000).emitWithAck('sign_transaction', {
+    const response = (await this.socket?.timeout(this.timeout).emitWithAck('sign_transaction', {
       network: params.network,
       transaction: transactionStr,
     })) as { signedTransaction?: string; error?: string };
